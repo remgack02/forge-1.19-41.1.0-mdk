54a62db6c3a6ff566b4866b0bc9c1efa44a0546e
remgack02 <gackremington@gmail.com>
2023-05-25 16:37:48 -0700

    important

--- src/main/java/net/remgack/testmod/TestMod.java
+++ src/main/java/net/remgack/testmod/TestMod.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TestMod.MOD_ID)
 public class TestMod {
+    //change
     public static final String MOD_ID = "testmod";
     private static final Logger LOGGER = LogUtils.getLogger();
     public ExampleMod() {
